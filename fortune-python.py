a14d5d47dc3566c8465c7b25b620f22aed62c539
lukalxn <lukalxn@gmail.com>
2023-04-29 22:36:31 -0400

    Wrote a fun little fortune telling python script using langchain for the text and tkinter for the gui.

---
+++ fortune-python.py
@@ -0,0 +1,11 @@
+***REMOVED***
+***REMOVED***
+
+llm=OpenAI(temperature=0.9***REMOVED***
+
+text="Divine the fortune of the user running this program and tell it in a poetic manner."
+
+window = tk.Tk(***REMOVED***
+greeting = tk.Label(text=llm(text***REMOVED******REMOVED***
+greeting.pack(***REMOVED***
+window.mainloop(***REMOVED***
