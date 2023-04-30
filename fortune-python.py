d43cb6871d7e90de5db34a686a4cb7721220ed12
lukalxn <lukalxn@gmail.com>
2023-04-29 22:44:02 -0400

    Modified the fortune program to tell your fortune in the style of arnold schawrzenegger. In testing, it told me to get to the choppa.

--- fortune-python.py
+++ fortune-python.py
@@ -3,7 +3,7 @@
 
 llm=OpenAI(temperature=0.9***REMOVED***
 
-text="Divine the fortune of the user running this program and tell it in a poetic manner."
+text="Read me my i ching fortune and tell it to me as if you're Arnold Schwarzenegger, former governor of california."
 
 window = tk.Tk(***REMOVED***
 greeting = tk.Label(text=llm(text***REMOVED******REMOVED***
