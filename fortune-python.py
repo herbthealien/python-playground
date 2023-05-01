098a6b9d09bb3fc0900c2bf9c47ae992efb2d866
lukalxn <lukalxn@gmail.com>
2023-04-30 22:24:08 -0400

    Fixed the prompting to stabilize test results. The AI now responds with a fortune told with Arnold Schwarzenegger's accent written out in the text. This is a great improvement over earier prompts, where either sometimes it would sound like the fortune was coming from a normal person or it would be far too unintelligible (such as when I asked for it to write phoenetically like Arnold Schwarzenegger, that created something so illegibile I could barely comprehend what it was actually trying to say). All in all, definitely worth a commit.

--- fortune-python.py
+++ fortune-python.py
@@ -4,13 +4,13 @@
 
 llm=OpenAI(temperature=0.9***REMOVED***
 
-text="You are Arnold Schwarzenegger and you're telling me my fortune."
+text="Tell me my future while talking like Arnold Schwarzenegger, writing your text out in his thick austrian accent."
 
 root=Tk(***REMOVED***
 root.title("The Fortunator"***REMOVED***
 root.geometry('640x480'***REMOVED***
 
-lbl = Label(root, text = llm(text***REMOVED******REMOVED***
+lbl = Label(root, text = llm(text***REMOVED***, wraplength=500, justify="center"***REMOVED***
 lbl.grid(***REMOVED***
-
+ 
 root.mainloop(***REMOVED***
