3bf9a39f076f2c681a78762996a79d553a2ed8b9
lukalxn <lukalxn@gmail.com>
2023-05-01 23:29:36 -0400

    Modified the GUI to include text formatting, a button to trigger the fortune response from langchain, and a picture of Arnold himself from the wikimedia commons to add to the effect.

--- fortune-python.py
+++ fortune-python.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import *
 from langchain.llms import OpenAI
+from PIL import Image, ImageTk
+from urllib.request import urlopen
+from io import BytesIO
 
 llm=OpenAI(temperature=0.9)
 
@@ -10,7 +13,25 @@ root=Tk()
 root.title("The Fortunator")
 root.geometry('640x480')
 
-lbl = Label(root, text = llm(text), wraplength=500, justify="center")
-lbl.grid()
- 
+url="https://upload.wikimedia.org/wikipedia/commons/c/cf/Schwarzenegger_Dec_2015.jpg"
+u=urlopen(url)
+raw_data=u.read()
+u.close()
+
+im = Image.open(BytesIO(raw_data))
+imr = im.resize((150,200))
+photo = ImageTk.PhotoImage(imr)
+
+label=Label(root, wraplength=500, justify="center", image=photo)
+label.image=photo
+label.pack(pady=20)
+
+arnold=Label(root, text="Arnold is more than happy to tell you your fortune, if you ask.", wraplength=500, justify="center")
+arnold.pack(pady=20)
+
+def fortune():
+   arnold["text"] = llm(text)
+
+B = Button(root, text='Ask Arnold For Your Fortune', command=fortune)
+B.pack(pady=20)
 root.mainloop()
