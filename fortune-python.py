607f027717ea966c1179d46c7c44ee1abff94dca
lukalxn <lukalxn@gmail.com>
2023-05-04 22:28:27 -0400

    Attempting to match the history of this and the github branch main. We'll see how this goes.

--- fortune-python.py
+++ fortune-python.py
@@ -1,37 +1,37 @@
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
+mport tkinter as tk
+from tkinter import *
+from langchain.llms import OpenAI
+from PIL import Image, ImageTk
+from urllib.request import urlopen
+from io import BytesIO
 
-llm=OpenAI(temperature=0.9***REMOVED***
+llm=OpenAI(temperature=0.9)
 
 text="Tell me my future while talking like Arnold Schwarzenegger, writing your text out in his thick austrian accent."
 
-root=Tk(***REMOVED***
-root.title("The Fortunator"***REMOVED***
-root.geometry('640x480'***REMOVED***
+root=Tk()
+root.title("The Fortunator")
+root.geometry('640x480')
 
 url="https://upload.wikimedia.org/wikipedia/commons/c/cf/Schwarzenegger_Dec_2015.jpg"
-u=urlopen(url***REMOVED***
-raw_data=u.read(***REMOVED***
-u.close(***REMOVED***
+u=urlopen(url)
+raw_data=u.read()
+u.close()
 
-im = Image.open(BytesIO(raw_data***REMOVED******REMOVED***
-imr = im.resize((150,200***REMOVED******REMOVED***
-photo = ImageTk.PhotoImage(imr***REMOVED***
+im = Image.open(BytesIO(raw_data))
+imr = im.resize((150,200))
+photo = ImageTk.PhotoImage(imr)
 
-label=Label(root, wraplength=500, justify="center", image=photo***REMOVED***
-***REMOVED***
-label.pack(pady=20***REMOVED***
+label=Label(root, wraplength=500, justify="center", image=photo)
+label.image=photo
+label.pack(pady=20)
 
-arnold=Label(root, text="Arnold is more than happy to tell you your fortune, if you ask.", wraplength=500, justify="center"***REMOVED***
-arnold.pack(pady=20***REMOVED***
+arnold=Label(root, text="Arnold is more than happy to tell you your fortune, if you ask.", wraplength=500, justify="center")
+arnold.pack(pady=20)
 
-def fortune(***REMOVED***:
-   arnold["text"] = llm(text***REMOVED***
+def fortune():
+   arnold["text"] = llm(text)
 
-B = Button(root, text='Ask Arnold For Your Fortune', command=fortune***REMOVED***
-B.pack(pady=20***REMOVED***
-root.mainloop(***REMOVED***
+B = Button(root, text='Ask Arnold For Your Fortune', command=fortune)
+B.pack(pady=20)
+root.mainloop()
