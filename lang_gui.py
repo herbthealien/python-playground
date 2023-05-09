75a817770efdaf310356181023dcf3c99b8fb8e7
lukalxn <lukalxn@gmail.com>
2023-05-09 00:49:15 -0400

    Added a lot of functionality to the GUI and set variables so that pending further development lesson plans can be built out for different areas of study. Excited to include calls to the AI tomorrow to use the logic built today to see how it might function.

--- lang_gui.py
+++ lang_gui.py
@@ -23,6 +23,9 @@ intro.pack(pady=20)
 
 inputtxt = Entry(root,
                    width = 20)
+
+inputskill = Entry(root,
+                   width = 20)
   
 inputtxt.pack()
 
@@ -46,9 +49,97 @@ s1 = "Sure, I'm happy to help you practice "
 
 s2 = ". Please specify your competency level."
 
+lang = inputtxt.get()
+
+
 def lang1():
    tag["text"] = s1 + inputtxt.get() + s2
+   inputtxt.pack_forget()
+   B.pack_forget()
+   intro.pack_forget()
+   inputskill.pack()
+   B2.pack(pady=20)
+
+def lang2():
+   tag["text"] = "Let's practice " + inputtxt.get() + " at the " + inputskill.get() + " level together. Please select an exercise to begin!"
+   inputskill.pack_forget()
+   B2.pack_forget()
+   greetings.pack()
+   food.pack()
+   work.pack()
+   culture.pack()
+   day2day.pack()
+
+def g1():
+   tag["text"] = "Let's practice greetings in " + inputtxt.get() + " together."
+   global subj
+   subj = "greetings"
+   greetings.pack_forget()
+   food.pack_forget()
+   work.pack_forget()
+   culture.pack_forget()
+   day2day.pack_forget()
+   ans.pack()
+
+def f1():
+   tag["text"] = "Let's practice dining and food in " + inputtxt.get() + " together."
+   global subj
+   subj = "food and dining"
+   greetings.pack_forget()
+   food.pack_forget()
+   work.pack_forget()
+   culture.pack_forget()
+   day2day.pack_forget()
+   ans.pack()
+
+def w1():
+   tag["text"] = "Let's practice workplace lingo in " + inputtxt.get() + " together."
+   global subj
+   subj = "talking in the workplace"
+   greetings.pack_forget()
+   food.pack_forget()
+   work.pack_forget()
+   culture.pack_forget()
+   day2day.pack_forget()
+   ans.pack()
+
+def c1():
+   tag["text"] = "Let's practice discussing popular culture in " + inputtxt.get() + " together."
+   global subj
+   subj = "popular culture"
+   greetings.pack_forget()
+   food.pack_forget()
+   work.pack_forget()
+   culture.pack_forget()
+   day2day.pack_forget()
+   ans.pack()
+
+def d2d1():
+   tag["text"] = "Let's practice talking about day to day life in " + inputtxt.get() + " together."
+   global subj
+   subj = "discussing day to day life"
+   greetings.pack_forget()
+   food.pack_forget()
+   work.pack_forget()
+   culture.pack_forget()
+   day2day.pack_forget()
+   ans.pack()
+
+def answer():
+   tag["text"] = "Let's keep learning " + inputtxt.get() + " together."
+
 
 B = Button(root, text='Specify Language', command=lang1)
+B2 = Button(root, text='Specify Competency', command=lang2)
+
+greetings = Button(root, text='Greetings', command=g1)
+food = Button(root, text='Food', command=f1)
+work = Button(root, text='Work', command=w1)
+culture = Button(root, text='Culture', command=c1)
+day2day = Button(root, text='Daily Life', command=d2d1)
+
+ans = Button(root, text='Answer', command=answer)
+
+
 B.pack(pady=20)
 root.mainloop()
